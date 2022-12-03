3373116890e214d2f8535b321a7e64421d7e9719
Daniel <fagundes.daniel191@gmail.com>
2022-12-03 17:24:08 -0300

    tentando colocar as listas de ab, fe

--- Node.py
+++ Node.py
@@ -1,83 +1,84 @@
 class Node:
     """A node state for water jug problem"""
+
     def __init__(self, start):
         self.state = start
         self.capacityJugA = 5
         self.capacityJugB = 3
-        self.visited = [[self.state]]
+        # self.visited = [self.state]
+        # self.visited.extend(visited)
 
-    def fillA(self):
-        if self.state[0] < self.capacityJugA and ([self.capacityJugA, self.state[1]] not in self.visited):
-            self.visited.append([self.capacityJugA, self.state[1]])
+    def fillA(self, visited):
+        if self.state[0] < self.capacityJugA and ([self.capacityJugA, self.state[1]] not in visited):
+            visited.append([self.capacityJugA, self.state[1]])
             return [self.capacityJugA, self.state[1]]
         else:
             return None
 
-
-    def fillB(self):
-        if self.state[1] < self.capacityJugB and ([self.state[0], self.capacityJugB] not in self.visited):
-            self.visited.append([self.state[0], self.capacityJugB])
+    def fillB(self, visited):
+        if self.state[1] < self.capacityJugB and ([self.state[0], self.capacityJugB] not in visited):
+            visited.append([self.state[0], self.capacityJugB])
             return [self.state[0], self.capacityJugB]
         else:
             return None
 
-    def emptyA(self):
-        if self.state[0] > 0 and ([0, self.state[1]] not in self.visited):
-            self.visited.append([0, self.state[1]])
+    def emptyA(self, visited):
+        if self.state[0] > 0 and ([0, self.state[1]] not in visited):
+            visited.append([0, self.state[1]])
             return [0, self.state[1]]
         else:
             return None
 
-    def emptyB(self):
-        if self.state[1] > 0 and ([self.state[0], 0] not in self.visited):
-            self.visited.append([self.state[0], 0])
+    def emptyB(self, visited):
+        if self.state[1] > 0 and ([self.state[0], 0] not in visited):
+            visited.append([self.state[0], 0])
             return [self.state[0], 0]
         else:
             return None
 
-    def pourAtoB(self):
+    def pourAtoB(self, visited):
         if self.state[0] > 0 and self.state[1] < self.capacityJugB:
             if self.state[0] + self.state[1] <= self.capacityJugB:
-                if ([0, self.state[0] + self.state[1]] not in self.visited):
-                    self.visited.append([0, self.state[0] + self.state[1]])
+                if ([0, self.state[0] + self.state[1]] not in visited):
+                    visited.append([0, self.state[0] + self.state[1]])
                     return [0, self.state[0] + self.state[1]]
                 else:
                     return None
             else:
-                if ([self.state[0] - (self.capacityJugB - self.state[1]), self.capacityJugB] not in self.visited):
-                    self.visited.append([self.state[0] - (self.capacityJugB - self.state[1]), self.capacityJugB])
+                if ([self.state[0] - (self.capacityJugB - self.state[1]), self.capacityJugB] not in visited):
+                    visited.append([self.state[0] - (self.capacityJugB - self.state[1]), self.capacityJugB])
                     return [self.state[0] - (self.capacityJugB - self.state[1]), self.capacityJugB]
                 else:
                     return None
         else:
             return None
 
-    def pourBtoA(self):
+    def pourBtoA(self, visited):
         if self.state[1] > 0 and self.state[0] < self.capacityJugA:
             if self.state[0] + self.state[1] <= self.capacityJugA:
-                if ([self.state[0] + self.state[1], 0] not in self.visited):
-                    self.visited.append([self.state[0] + self.state[1], 0])
+                if ([self.state[0] + self.state[1], 0] not in visited):
+                    visited.append([self.state[0] + self.state[1], 0])
                     return [self.state[0] + self.state[1], 0]
                 else:
                     return None
             else:
-                if ([self.capacityJugA, self.state[1] - (self.capacityJugA - self.state[0])] not in self.visited):
-                    self.visited.append([self.capacityJugA, self.state[1] - (self.capacityJugA - self.state[0])])
+                if ([self.capacityJugA, self.state[1] - (self.capacityJugA - self.state[0])] not in visited):
+                    visited.append([self.capacityJugA, self.state[1] - (self.capacityJugA - self.state[0])])
                     return [self.capacityJugA, self.state[1] - (self.capacityJugA - self.state[0])]
                 else:
                     return None
         else:
             return None
 
-    def applyOperators(self):
-        return [self.fillA(), self.fillB(), self.emptyA(), self.emptyB(), self.pourAtoB(), self.pourBtoA()]
-
+    def applyOperators(self, visited):
+        return [self.fillA(visited), self.fillB(visited), self.emptyA(visited), self.emptyB(visited),
+                self.pourAtoB(visited), self.pourBtoA(visited)]
 
     def BFS(self, goal):
         """Breadth First Search"""
         queue = []
         queue.append([self.state])
-
+        visited = []
         while queue:
             path = queue.pop(0)
             node = path[-1]
@@ -87,7 +88,7 @@ class Node:
                 return path
             else:
                 myState = Node(node)
-                listOfList = myState.applyOperators()
+                listOfList = myState.applyOperators(visited)
                 for i in listOfList:
                     if i and i not in path:
                         newPath = list(path)
@@ -98,65 +99,51 @@ class Node:
         """Depth First Search"""
         stack = []
         stack.append([self.state])
-
+        visited = []
+        closed = []
         while stack:
             path = stack.pop()
             node = path[-1]
             jugA = node[0]
 
             if jugA == goal:
+                print("open: ", stack)
+                print("closed: ", closed)
                 return path
             else:
                 myState = Node(node)
-                listOfList = myState.applyOperators()
+                listOfList = myState.applyOperators(visited)
                 for i in listOfList:
                     if i and i not in path:
                         newPath = list(path)
                         newPath.append(i)
                         stack.append(newPath)
+                closed.append(myState.state)
+    print("")
 
 
     def backtracking(self, goal):
         """iterative Backtracking"""
         stack = []
         stack.append([self.state])
-
+        closed = []
         while stack:
             path = stack.pop()
             node = path[-1]
+            print("node: ", node)
             jugA = node[0]
 
             if jugA == goal:
+                print("open: ", stack)
+                print("closed: ", closed)
                 return path
             else:
                 myState = Node(node)
-                listOfList = myState.applyOperators()
+                listOfList = myState.applyOperators(visited=[])
                 for i in listOfList:
                     if i and i not in path:
                         newPath = list(path)
                         newPath.append(i)
                         stack.append(newPath)
-                        break
-
-    def backtracking2(self, goal):
-        """testando backtracking"""
-        stack = []
-        stack.append([self.state])
-
-        while stack:
-            path = stack.pop()
-            lastState = path[-1]
-            jugA = lastState[0]
-
-            if jugA == goal:
-                return path
-            else:
-                listOfList = self.applyOperators() # para a funcao funcionar tem que alterar a logica das regras, remover o self.visited.append delas e colocar aqui
-                for i in listOfList:
-                    if i and i not in path:
-                        newPath = list(path)
-                        newPath.append(i)
-                        stack.append(newPath)
-                        self.visited.append(i) # substituindo por esse append aqui
-                        self.state = i
+                        closed.append(myState.state)
                         break
--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 from Node import Node
 
 if __name__ == '__main__':
-    print("BFS")
-    node = Node([0, 0])
-    print(node.BFS(1))
+    # print("BFS")
+    # node = Node([0, 0])
+    # print("Solution: ", str(node.BFS(1)))
 
     print("DFS")
     node = Node([0, 0])
-    print(node.DFS(1))
+    print("Solution: ", str(node.DFS(1)))
 
-    print("Backtracking")
-    node = Node([0, 0])
-    print(node.backtracking(1))
+    # print("Backtracking")
+    # node = Node([0, 0])
+    # print("Solution: " + str(node.backtracking(1)))
 
