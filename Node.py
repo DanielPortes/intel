d67eb9ded357a918f9c8d4936cbb64e1f1bd3d72
Daniel <fagundes.daniel191@gmail.com>
2022-12-03 15:27:16 -0300

    .

--- Node.py
+++ Node.py
@@ -1,134 +1,162 @@
 class Node:
+    """A node state for water jug problem"""
     def __init__(self, start):
-        self.estado = start
-        self.capacidadeJarroA = 5
-        self.capacidadeJarroB = 3
-        self.visitados = []
+        self.state = start
+        self.capacityJugA = 5
+        self.capacityJugB = 3
+        self.visited = [[self.state]]
 
-    def encheA(self):
-        if self.estado[0] < self.capacidadeJarroA and ([self.capacidadeJarroA, self.estado[1]] not in self.visitados):
-            self.visitados.append([self.capacidadeJarroA, self.estado[1]])
-            return [self.capacidadeJarroA, self.estado[1]]
+    def fillA(self):
+        if self.state[0] < self.capacityJugA and ([self.capacityJugA, self.state[1]] not in self.visited):
+            self.visited.append([self.capacityJugA, self.state[1]])
+            return [self.capacityJugA, self.state[1]]
         else:
             return None
 
 
-    def encheB(self):
-        if self.estado[1] < self.capacidadeJarroB and ([self.estado[0], self.capacidadeJarroB] not in self.visitados):
-            self.visitados.append([self.estado[0], self.capacidadeJarroB])
-            return [self.estado[0], self.capacidadeJarroB]
+    def fillB(self):
+        if self.state[1] < self.capacityJugB and ([self.state[0], self.capacityJugB] not in self.visited):
+            self.visited.append([self.state[0], self.capacityJugB])
+            return [self.state[0], self.capacityJugB]
         else:
             return None
 
-    def esvaziaA(self):
-        if self.estado[0] > 0 and ([0, self.estado[1]] not in self.visitados):
-            self.visitados.append([0, self.estado[1]])
-            return [0, self.estado[1]]
+    def emptyA(self):
+        if self.state[0] > 0 and ([0, self.state[1]] not in self.visited):
+            self.visited.append([0, self.state[1]])
+            return [0, self.state[1]]
         else:
             return None
 
-    def esvaziaB(self):
-        if self.estado[1] > 0 and ([self.estado[0], 0] not in self.visitados):
-            self.visitados.append([self.estado[0], 0])
-            return [self.estado[0], 0]
+    def emptyB(self):
+        if self.state[1] > 0 and ([self.state[0], 0] not in self.visited):
+            self.visited.append([self.state[0], 0])
+            return [self.state[0], 0]
         else:
             return None
 
-    def AparaB(self):
-        if self.estado[0] > 0 and self.estado[1] < self.capacidadeJarroB:
-            if self.estado[0] + self.estado[1] <= self.capacidadeJarroB:
-                if ([0, self.estado[0] + self.estado[1]] not in self.visitados):
-                    self.visitados.append([0, self.estado[0] + self.estado[1]])
-                    return [0, self.estado[0] + self.estado[1]]
+    def pourAtoB(self):
+        if self.state[0] > 0 and self.state[1] < self.capacityJugB:
+            if self.state[0] + self.state[1] <= self.capacityJugB:
+                if ([0, self.state[0] + self.state[1]] not in self.visited):
+                    self.visited.append([0, self.state[0] + self.state[1]])
+                    return [0, self.state[0] + self.state[1]]
                 else:
                     return None
             else:
-                if ([self.estado[0] - (self.capacidadeJarroB - self.estado[1]), self.capacidadeJarroB] not in self.visitados):
-                    self.visitados.append([self.estado[0] - (self.capacidadeJarroB - self.estado[1]), self.capacidadeJarroB])
-                    return [self.estado[0] - (self.capacidadeJarroB - self.estado[1]), self.capacidadeJarroB]
+                if ([self.state[0] - (self.capacityJugB - self.state[1]), self.capacityJugB] not in self.visited):
+                    self.visited.append([self.state[0] - (self.capacityJugB - self.state[1]), self.capacityJugB])
+                    return [self.state[0] - (self.capacityJugB - self.state[1]), self.capacityJugB]
                 else:
                     return None
         else:
             return None
 
-    def BparaA(self):
-        if self.estado[1] > 0 and self.estado[0] < self.capacidadeJarroA:
-            if self.estado[0] + self.estado[1] <= self.capacidadeJarroA:
-                if ([self.estado[0] + self.estado[1], 0] not in self.visitados):
-                    self.visitados.append([self.estado[0] + self.estado[1], 0])
-                    return [self.estado[0] + self.estado[1], 0]
+    def pourBtoA(self):
+        if self.state[1] > 0 and self.state[0] < self.capacityJugA:
+            if self.state[0] + self.state[1] <= self.capacityJugA:
+                if ([self.state[0] + self.state[1], 0] not in self.visited):
+                    self.visited.append([self.state[0] + self.state[1], 0])
+                    return [self.state[0] + self.state[1], 0]
                 else:
                     return None
             else:
-                if ([self.capacidadeJarroA, self.estado[1] - (self.capacidadeJarroA - self.estado[0])] not in self.visitados):
-                    self.visitados.append([self.capacidadeJarroA, self.estado[1] - (self.capacidadeJarroA - self.estado[0])])
-                    return [self.capacidadeJarroA, self.estado[1] - (self.capacidadeJarroA - self.estado[0])]
+                if ([self.capacityJugA, self.state[1] - (self.capacityJugA - self.state[0])] not in self.visited):
+                    self.visited.append([self.capacityJugA, self.state[1] - (self.capacityJugA - self.state[0])])
+                    return [self.capacityJugA, self.state[1] - (self.capacityJugA - self.state[0])]
                 else:
                     return None
         else:
             return None
 
-    def operadores(self):
-        return [self.encheA(), self.encheB(), self.esvaziaA(), self.esvaziaB(), self.AparaB(), self.BparaA()]
+    def applyOperators(self):
+        return [self.fillA(), self.fillB(), self.emptyA(), self.emptyB(), self.pourAtoB(), self.pourBtoA()]
 
 
-    def BFS(self, objetivoY):
+    def BFS(self, goal):
+        """Breadth First Search"""
         queue = []
-        queue.append([self.estado])
+        queue.append([self.state])
 
         while queue:
             path = queue.pop(0)
             node = path[-1]
-            y = node[1]
+            jugA = node[0]
 
-            if y == objetivoY:
+            if jugA == goal:
                 return path
             else:
-                objEstado = Node(node)
-                regras = objEstado.operadores()
-                for i in regras:
+                myState = Node(node)
+                listOfList = myState.applyOperators()
+                for i in listOfList:
                     if i and i not in path:
                         newPath = list(path)
                         newPath.append(i)
                         queue.append(newPath)
 
-    def DFS(self, objetivoY):
+    def DFS(self, goal):
+        """Depth First Search"""
         stack = []
-        stack.append([self.estado])
+        stack.append([self.state])
 
         while stack:
             path = stack.pop()
             node = path[-1]
-            y = node[1]
+            jugA = node[0]
 
-            if y == objetivoY:
+            if jugA == goal:
                 return path
             else:
-                objEstado = Node(node)
-                regras = objEstado.operadores()
-                for i in regras:
+                myState = Node(node)
+                listOfList = myState.applyOperators()
+                for i in listOfList:
                     if i and i not in path:
                         newPath = list(path)
                         newPath.append(i)
                         stack.append(newPath)
 
 
-    def backtracking(self, objetivoY):
+    def backtracking(self, goal):
+        """iterative Backtracking"""
         stack = []
-        stack.append([self.estado])
+        stack.append([self.state])
 
         while stack:
             path = stack.pop()
             node = path[-1]
-            y = node[1]
+            jugA = node[0]
 
-            if y == objetivoY:
+            if jugA == goal:
                 return path
             else:
-                objEstado = Node(node)
-                regras = objEstado.operadores()
-                for i in regras:
+                myState = Node(node)
+                listOfList = myState.applyOperators()
+                for i in listOfList:
                     if i and i not in path:
                         newPath = list(path)
                         newPath.append(i)
                         stack.append(newPath)
+                        break
+
+    def backtracking2(self, goal):
+        """testando backtracking"""
+        stack = []
+        stack.append([self.state])
+
+        while stack:
+            path = stack.pop()
+            lastState = path[-1]
+            jugA = lastState[0]
+
+            if jugA == goal:
+                return path
+            else:
+                listOfList = self.applyOperators() # para a funcao funcionar tem que alterar a logica das regras, remover o self.visited.append delas e colocar aqui
+                for i in listOfList:
+                    if i and i not in path:
+                        newPath = list(path)
+                        newPath.append(i)
+                        stack.append(newPath)
+                        self.visited.append(i) # substituindo por esse append aqui
+                        self.state = i
+                        break
